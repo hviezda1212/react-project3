f77516780c3c106fce98797222bd8e5abd5817fd
KIMMIJU <hviezda1212@gmail.com>
2024-03-27 23:16:03 +0900

    login 페이지 이동

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button, Form, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({setAuthenticate}) => {
+  const navigate = useNavigate ();
   const loginUser = (event)=> {
     event.preventDefault();
+    setAuthenticate(true);
+    navigate("/");
   }
   return (
     <Container>
