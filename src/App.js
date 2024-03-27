b40522dc27924f78e0355a2becabd98e568fa8a6
KIMMIJU <hviezda1212@gmail.com>
2024-03-27 21:47:31 +0900

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import ProductAll from './page/ProductAll';
-import Login from './page/Login';
-import ProductDetail from './page/ProductDetail';
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
+import Navbar from "./component/Navbar"
 
 function App() {
   return (
-    <div >
-<Routes>
-  <Route path = "/" element ={<ProductAll/>}/>
-  <Route path = "/login" element ={<Login/>}/>
-  <Route path = "/product/:id" element ={<ProductDetail/>}/>
-</Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
     </div>
   );
 }
