e9e172474bc8d288dc2c15faa893c6f1faa39f8c
KIMMIJU <hviezda1212@gmail.com>
2024-03-28 01:31:36 +0900

    navbar 수정

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
@@ -35,17 +35,29 @@ const Navbar = () => {
 
   return (
     <div>
-      <div>
-        <div className="login-button" onClick={goToLogin}>
-          <FontAwesomeIcon icon={faUser}/>
-          <div>로그인</div>
-        </div>
+      <div className="top-area">
       </div>
       <div className="nav-section">
+      <div className="left-button-area" onClick={goToLogin}>
+          <div>고객 서비스</div>
+          <div>뉴스레터</div>
+          <div>매장 찾기</div>
+
+        </div>
         <img onClick={goToMain}
           width={100}
           src="https://i.namu.wiki/i/25UU_-oz7gEmFHCa3f3du7joUUaFzkbyiAC6iR0Ee-JvdYRHfvB8zKGB2GsOH9sREEY4_9GMNGiEw-gvjchx_UxTBpKl-XIRyG63z9-rk2raoBgN4LJou5LdXhIXrx9QQtV7MoA6nCDqJhtyW7K2Nw.svg"
         />
+                <div className="right-button-area">
+        <div className="login-button" onClick={goToLogin}>
+          <FontAwesomeIcon icon={faUser}/>
+          <div>로그인</div>
+        </div>
+        <div className="like-button" onClick={goToLogin}>
+          <FontAwesomeIcon icon={faHeart}/>
+          <div>즐겨찾기</div>
+        </div>
+        </div>
       </div>
       <div className="menu-area">
         <ul className="menu-list">
