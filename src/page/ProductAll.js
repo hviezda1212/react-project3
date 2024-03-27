6b31af349d85dcd8029686bdd9298f5ac3613c14
KIMMIJU <hviezda1212@gmail.com>
2024-03-27 22:45:57 +0900

    json 경로 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -5,7 +5,7 @@ import ProductCard from "../component/ProductCard";
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async () => {
-    let url = ` http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/hviezda1212/react-project3/products`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
